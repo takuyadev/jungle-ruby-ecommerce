033d96911163b1b944a990855b188bc50d118f3b
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-05-04 13:31:14 -0700

    Add authntication and login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,22 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to "/"
+    else
+      redirect_to [:new, :user]
+    end
   end
 
   def new
-    @user = User.new
+    @user = User.new()
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
